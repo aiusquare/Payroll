592323a4a04589763e2965f7807c7554fbeff56f
aiusquare <ahmadibrahimusmanmkk@gmail.com>
2020-02-29 03:42:55 -0800

    time analysis

--- Payroll system/src/CustomerRecord.java
+++ Payroll system/src/CustomerRecord.java
@@ -1,12 +1,14 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
 
 
-public class CustomerRecord implements Serializable{
+public class CustomerRecord extends ArrayList{
 	String employeeCode;
 	String employeeName;
 	int employeeLevel;
 	int employeeRate;
-	int totalHoursWorked[];
 	
 	public CustomerRecord(String name, String code, int level, int rate) {
 		employeeName= name;
@@ -16,6 +18,24 @@ public class CustomerRecord implements Serializable{
 	}
 	
 	public CustomerRecord(){
+		
+	}
+	
+	public void setSignInTime(String[] sInTime){
+		int totalTime=0;
+		for(int i=0;i<sInTime.length;i++){
+			String time = sInTime[i];
+			String hour = time.substring(0, time.indexOf(":"));
+			String minute =time.substring(time.indexOf(":")+1);
+			
+			
+			
+			System.out.println(hour);
+			System.out.println(minute);
+		}
+	}
+	
+	public void setSignOutTime(String[] sInTime){
 		
 	}
 }
--- Payroll system/src/MainClasss.java
+++ Payroll system/src/MainClasss.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
 
-
 public class MainClasss {
 	public static void main(String[] args){
 		String action="";
@@ -45,13 +44,30 @@ public class MainClasss {
 					if(c.employeeCode.equalsIgnoreCase(code)){
 						System.out.println("Employee name: " +c.employeeName);
 						System.out.println("Employee salary rate: " + c.employeeRate);
+						
+						System.out.println("hours worked througth the week");
+						String day = "";
+						String[] days ={"MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"};
+						String[] signInTime = new String[5];
+						String[] signOutTime = new String[5];
+						
+						for(int j =0; j<signInTime.length;j++){
+							System.out.println("sign in time on: "+ days[j]);
+							signInTime[j] = inputLine.readLine();
+							System.out.println("sign out time on: "+days[j]);
+							signOutTime[j] = inputLine.readLine();
+						}
+						c.setSignInTime(signInTime);
+						c.setSignOutTime(signOutTime);
 					}
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
+				//System.out.print(e.getMessage());
 			}
 			
 			break;
 		}
 	}
 }  
+  
